17c28fd1c583a562eec258a5c523d4bdac44aa78
gabrielapl <gabrielapl@unipam.edu.br>
2023-03-29 00:26:32 -0300

    complete structure home page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { Timer, Scroll } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/ignite-logo.svg'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
